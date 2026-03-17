6f886745aa4efac97eca5879d896e81bb7488f91
bekzodrakhmonov7 <bekzodrakhmonov7@gmail.com>
2026-03-17 23:59:38 +0200

    added creation of todo item

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sqlalchemy.exc import IntegrityError
 from sqlmodel import SQLModel, select
 
 from db import SessionDep, db_engine
-from models import UserBase, UserLogin, UserPublic, Users
+from models import TodoBase, TodoPublic, Todos, UserBase, UserLogin, UserPublic, Users
 from security import (
     create_access_token,
     decode_access_token,
@@ -94,3 +94,39 @@ def get_current_user(
         )
 
     return db_user
+
+
+@app.post("/todos", response_model=TodoPublic)
+def create_todo(
+    session: SessionDep,
+    todo: TodoBase,
+    credentials: HTTPAuthorizationCredentials = Depends(bearer),
+):
+    token = credentials.credentials
+
+    payload = decode_access_token(token)
+    if not payload:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Bearer token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    token_username = payload.get("username")
+    if not token_username:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Token payload is invalid"
+        )
+    statement = select(Users.id).where(Users.username == token_username)
+    db_user_id = session.exec(statement).first()
+    db_todo = Todos.model_validate(todo)
+    db_todo.user_id = db_user_id
+    try:
+        session.add(db_todo)
+        session.commit()
+        session.refresh(db_todo)
+    except IntegrityError:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Error while creating todo",
+        )
+    return db_todo
